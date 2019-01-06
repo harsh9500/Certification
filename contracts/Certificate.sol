d6e9c21b5bde842bef67da63f324a7b8744c8638
harshjain <harsh9500@rediffmail.com>
2019-01-06 05:52:52 +0530

    Final commit

--- contracts/Certificate.sol
+++ contracts/Certificate.sol
@@ -4,21 +4,27 @@ contract Certificate {
     struct Person {
         address id;
         uint no;
+    }
 
+    struct Verifier {
+        address id;
+        uint no;
     }
 
     struct certi {
         string hash;
         string certificate_string;
     }
-        
+
+    mapping(address => certi[10]) public verifiers;
+
     mapping(address => certi[10]) public certificates;
     
     mapping(address => uint) public keyDetails;
     
-    mapping(uint => bytes32) public certihashes;
-    
     mapping(uint => Person) public Persons;
+
+    mapping(uint => Verifier) public Verifiers;
 
     uint n=3127;
     uint phin=3016;
@@ -26,8 +32,8 @@ contract Certificate {
     uint remainder;
     uint pk;
     
-    uint certicount=0;
     uint public personcount=0;
+    uint public vercount=0;
 
     function Certificate() public payable{
         keyDetails[msg.sender]=3;
@@ -36,8 +42,9 @@ contract Certificate {
         keyDetails[0xA5814552D0c2A50fDC99D3e9b419aC5c04B975Ea]=200;
         keyDetails[0x916c40C84d5A0FD4a3d91f025553f8592D9eA003]=200;
         addPerson(msg.sender);
-        addtoArray(msg.sender,"ABCD1234","sdfdsfgfg1234");
-        addtoArray(msg.sender,"PQRS1234","sfsdgdg1234");
+        addVerifier(msg.sender);
+        // addtoArray(msg.sender,"ABCD1234","sdfdsfgfg1234");
+        // addtoArray(msg.sender,"PQRS1234","sfsdgdg1234");
     }
 
     function addPerson(address _p) private{
@@ -45,7 +52,12 @@ contract Certificate {
         Persons[personcount] = Person(_p,0);
     }
     
-    function HashCalculation(address p,string certi, uint private_key) public payable{    
+    function addVerifier(address _p) private{
+        vercount ++;
+        Verifiers[vercount] = Verifier(_p,0);
+    }
+
+    function HashCalculation(uint private_key) public payable{    
         
         pk = keyDetails[msg.sender];
         remainder = (pk*private_key)%phin;
@@ -53,8 +65,6 @@ contract Certificate {
         {
             revert();
         }
-        certicount++;
-        certihashes[certicount]=keccak256(certi);
 
     }
     
@@ -77,6 +87,23 @@ contract Certificate {
         
     }
 
+    function addCertitoVerifier(address p, string hash, string certi_string) public payable{
 
+    for(uint i=1;i<=vercount;i++)
+        {
+            if(Verifiers[i].id==p)
+            {
+                break;
+            }
+        }
+    if(i>vercount)
+        {
+            revert();
+        }
+
+        verifiers[p][Verifiers[i].no]=certi(hash,certi_string);
+        Verifiers[i].no+=1;
+        
+    }
     
 }
--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ App = {
   web3Provider: null,
   contracts: {},
   account: '0x0',
-  fhash:" ",
+  temp:0,
 
   init: function() {
     return App.initWeb3();
@@ -27,24 +27,8 @@ App = {
       App.contracts.Certificate = TruffleContract(certificate);
       
       App.contracts.Certificate.setProvider(App.web3Provider);
-      console.log(App.contracts.Certificate)
-      //App.listenForEvents();
 
       return App.render();
-    });
-  },
-
-  
-  listenForEvents: function() {
-    App.contracts.Certificate.deployed().then(function(instance) {
-      // Restart Chrome if unable to receive this event
-      instance.hashcalculated({}, {
-        fromBlock: 0,
-        toBlock: 'latest'
-      }).watch(function(error, event) {
-        console.log("event triggered", event)
-        App.render();
-      });
     });
   },
 
@@ -58,11 +42,14 @@ App = {
     for(var k=0;k<c;k++)
     {
             
-            
+            App.temp = k;
             Instance.certificates(add,k).then(function(arr){
-              
-            Template= "<tr><th>" + arr[0] + "</th><td>"; 
-            Template+= arr[1] + "</td></tr>";
+            Template = "<div class='card'>";
+            Template += "<img class='farhan' src='" + arr[1] + "' class='card-img-top' style='width:40%'>";
+            Template += "<div class='container'><h4><b>Hash value</b></h4><p class='secimage'>"
+            Template += arr[0]
+            Template += "</p><input type='text' class='col-6 verifieraddr'><br/><input type='button' class='btn btn-primary verifier' name='"+App.temp+"' value='Send' onClick='App.verifier("+k+")'></div></div>"
+
             
             Results.append(Template);
       })
@@ -71,6 +58,33 @@ App = {
 })
   },
 
+
+  displayVerifier:function(c,add){
+
+    App.contracts.Certificate.deployed().then(function(instance) {
+      Instance=instance;
+      var Results = $("#Results2");
+      var Template="";
+    for(var k=0;k<c;k++)
+    {
+            
+            App.temp = k;
+            Instance.verifiers(add,k).then(function(arr){
+            Template = "<div class='cardv'>";
+            Template += "<img class='farhanv' src='" + arr[1] + "' class='card-img-top' style='width:40%'>";
+            Template += "<div class='container'><h4><b>Hash value</b></h4><p class='secimagev'>"
+            Template += arr[0]
+            Template += "</p><p>Verified</p></div></div>"
+
+            
+            Results.append(Template);
+      })
+    }    
+})
+  },
+
+
+  
   readFile: function(){
     var certi;
     if (this.files && this.files[0]) {
@@ -101,6 +115,37 @@ App = {
     
   },
 
+
+  readFilev: function(){
+    var certi;
+    if (this.files && this.files[0]) {
+        
+        var FR= new FileReader();
+        
+        FR.onload = function(e) {
+
+          $('#imgv').attr('src', e.target.result);
+          $("#b64v").html(e.target.result);
+          certi=e.target.result;
+          certi.toString()
+         
+          var hash = 0, i, chr;
+    
+        for (i = 0; i < certi.length; i++) {
+      chr   = certi.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0; 
+    }
+
+      
+      $("#string_hashv").html(hash.toString()); 
+        } 
+  
+        FR.readAsDataURL( this.files[0] );
+      }
+    
+  },
+
   render: function() {
     var Instance;
     var loader = $("#loader");
@@ -111,11 +156,12 @@ App = {
 
 
   $("#inp").on("change", App.readFile);
+  $("#inpv").on("change", App.readFilev);
+
 
     web3.eth.getCoinbase(function(err, account) {
       if (err === null) {
         App.account = account;
-        $("#accountAddress").html("Your Account: " + account);
       }
     });
 
@@ -140,7 +186,8 @@ App = {
                  
             add=(result[0]);
             c=(result[1]["c"][0]);
-            App.display(c,add);    
+            App.display(c,add);
+            App.displayVerifier(c,add);
               
         })
 
@@ -158,22 +205,20 @@ App = {
         
       var certi;
 
-      console.log($("#b64").text());
+      
       certi=$("#b64").text();
       addr=$("#address").val();
       pk=$("#pk").val();
       string_hash=$("#string_hash").text().toString();
-      console.log(certi);
-      console.log(string_hash);
-      console.log(typeof(string_hash));
+      
       App.contracts.Certificate.deployed().then(function(instance) {
         
         Instance=instance
-        console.log("kuch toh");
-
-        Instance.HashCalculation(addr,certi,pk).then(function(result) {
         
-        console.log("Inside Hash");
+
+        Instance.HashCalculation(pk).then(function(result) {
+        
+        
 
       }).catch(function(err) {
         console.error(err);
@@ -186,36 +231,150 @@ App = {
       function(error, hash){
         if(error)
           console.log(error);
-          console.log("abhi error");
-        console.log("okay");
+          
 
           Instance.addtoArray(addr,hash,certi).then(function(r){
-            console.log(r);
-            console.log(r["tx"]);
-            console.log(certi);
-            console.log("sent");
-            console.log(hash);
-            console.log(addr);
+            
+            location.reload();
           }).catch(function(err) {
         console.error(err);
         });   
       })
       
-          
-      
-
-        //$("#content").hide();
-        //$("#loader").show();
-
-      
 
       });
-    }
+    },
+     verifier:function(e,k){
+    // console.log($base[e]);
+    // console.log($img[e]);
+    var addr = $('.verifieraddr').eq(e).val();
+   var base = $base[e];
+   var hash = $img[e];
+
+App.contracts.Certificate.deployed().then(function(instance) {
+        
+        Instance=instance
+        
+
+        Instance.addCertitoVerifier(addr,hash,base).then(function(result) {
+        
+      }).catch(function(err) {
+        console.error(err);
+        });
+})
+ },
+
+
+ verify:function(){
+    var string_hash = $("#string_hashv").val();
+    var txnhash = $("#txnhash").val();
+    var comp;
+    
+
+    web3.eth.getTransaction(
+          txnhash,
+      function(error, hash){
+        if(error)
+          console.log(error);
+          
+        
+      comp = web3.eth.abi.decodeParameter('string',hash["input"]);
+      console.log(comp);
+        
+      });
+
+      if(comp==string_hash)
+      {
+        $("#result").html("Verified!!");
+        $("#result").show();
+      }
+      else
+      {
+        $("#result").html("Fake!!");
+        $("#result").show(); 
+      }
+ }
 
 };
 //End of App Object
+
 $(function() {
   $(window).load(function() {
     App.init();
   });
+  $('#verifdiv').hide();
+  $base = []
+  $img = []
+  $('#transition').on('click', function(){
+    if (App.fhash==0){
+      App.fhash=1
+    }
+    else{
+      App.fhash=0
+    }
+    $('.hide').addClass('set').removeClass('hide');
+    $('.seek').addClass('hide').removeClass('seek');  
+    $('.set').addClass('seek').removeClass('set');  
+    $images = $('.farhan');
+    
+    if ($images) {
+      $('.farhan').each(function( index ) {
+        $base.push($(this).attr('src'));
+      });
+      $('.secimage').each(function( index ) {
+        $img.push($(this).text());
+      });
+    }
+    $count = 0;
+    $('.card').each(function(index){
+      $str = "App.verifier("+$count+")";
+      $(this).children().eq(1).children().eq(4).attr('onClick',$str);
+      $count = $count + 1;
+    })
+  });
+  
+  $('.verifier').on("click", function(event) {
+    
+    event.preventDefault();
+    $index = $(this).attr('name');
+
+  });
+  $('#veri').on('click', function(event){
+    $('#content').hide();
+    $('#verifdiv').show();
+    App.contracts.Certificate.deployed().then(function(instance) {
+      
+      Instance = instance;
+      
+      return Instance.vercount();
+
+    }).then(function(Count){
+      
+      var Results = $("#Results2");
+      Results.empty(); 
+
+      for (var i = 1; i <= Count ; i++) {
+        var c;
+        var add;
+        var k;
+        var Template = "";
+
+        Instance.Verifiers(i).then(function(result){
+                 
+            add=(result[0]);
+            c=(result[1]["c"][0]);
+            App.displayVerifier(c,add);
+              
+        })
+
+          
+        } 
+      
+      });
+  });
+  $count = 0;
+
+
+
+
 });
